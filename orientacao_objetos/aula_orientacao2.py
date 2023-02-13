7f19deb1a116dbaffe12424e5bd9d9eef99a5b54
Joao Ramos <joao.fernando.ramos23@gmail.com>
2023-02-13 15:42:26 -0300

    Adicionando pasta de orientação ao objeto me python

---
+++ orientacao_objetos/aula_orientacao2.py
@@ -0,0 +1,40 @@
+class Automovel:
+
+    def __init__(self):
+        pass
+
+    def alterar_placa(self,nova):
+        self.placa  = nova
+        return self.placa
+
+    def Acelerar(self):
+        pass
+
+    def Parar(self):
+        pass
+
+class Carro(Automovel):
+    def __init__(self):
+        pass
+
+    combustivel = None
+    cor = None
+    placa = None
+
+novo_carro = Carro()
+novo_carro.combustivel = "Gasolina"
+novo_carro.cor = "Vermelho"
+novo_carro.placa = "OUO-9090"
+
+exibir = novo_carro.__dict__
+
+for key,value in exibir.items():
+    print(f"{key} -> {value}")
+
+print()
+
+novo_carro.alterar_placa("OUO-0909")
+
+for key,value in exibir.items():
+    print(f"{key} -> {value}")
+
